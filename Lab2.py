b7d9ec0a7801c9f3ceb603937cba1232922f6c0a
Emmanuel <emmanuelongsh25.24@ichat.sp.edu.sg>
2025-05-08 14:53:17 +0800

    Added Lab2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,47 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print('Enter some numbers separated by commas (e.g. 5, 67, 32)')
+
+def calc_average_temperature(num_list):
+    Average_Temp = sum(num_list)/len(num_list)
+    return Average_Temp
+
+def calc_min_max_temperature(num_list):
+    Min_Temp = int(min(num_list))
+    Max_Temp = int(max(num_list))
+    return (Min_Temp,Max_Temp)
+
+def calc_median_temperature(num_list):
+    sorted_list = sorted(num_list)
+    length_list = len(sorted_list)
+    
+    if length_list % 2 == 0:
+        median_number = length_list + 1 / 2
+
+    else:
+        median_number = length_list/2
+
+    return median_number
+
+
+def get_user_input():
+    user_input = input()
+    splitted_numbers = user_input.split(',')
+    num_list = [float(num.strip()) for num in splitted_numbers]
+    return num_list
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+    avg = calc_average_temperature(num_list)
+    min_max = calc_min_max_temperature(num_list)
+    median = calc_median_temperature(num_list)
+
+    print("The Average Temperature is : ", avg)
+    print("The Minimum and Maximum Temperatures : ", min_max, "respectively")
+    print("The Median Temperature : " , median)
+    
+if __name__ == "__main__":
+    main()
+
